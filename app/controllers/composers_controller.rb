f84da3a75d9acd6fad5c5a303dc009cdf8f57412
Michael Brandt <msbrandt00@gmail.com>
2022-02-02 11:37:23 -0700

    Feature: '/composers/pieces'

--- app/controllers/composers_controller.rb
+++ app/controllers/composers_controller.rb
@@ -6,4 +6,8 @@ class ComposersController < ApplicationController
   def show
     @composer = Composer.find(params[:id])
   end
+
+  def compositions
+    @own_works = Composer.find(params[:composer_id]).pieces
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   get '/pieces/:id', to: 'pieces#show'
   get '/composers', to: 'composers#index'
   get '/composers/:id', to: 'composers#show'
+  get '/composers/:composer_id/pieces', to: 'composers#compositions'
   get '/', to: 'welcome#index'
 end
+
+# User Story 5, Parent Children Index (x2)
+#
+# As a visitor
+# When I visit '/parents/:parent_id/child_table_name'
+# Then I see each Child that is associated with that Parent with each Child's attributes:

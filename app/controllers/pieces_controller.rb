041332fa01696b7dc38b058a561b456499b098f9
Michael Brandt <msbrandt00@gmail.com>
2022-02-04 15:11:50 -0700

    Controllers and view changes for editing Piece

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -13,9 +13,21 @@ class PiecesController < ApplicationController
     redirect_to "/composers/#{composer.id}/pieces"
   end
 
+  def edit
+    @piece = Piece.find(params[:id])
+  end
+
+  def update
+    piece = Piece.find(params[:id])
+    piece.update(piece_params[:piece])
+    piece.save
+    redirect_to "/pieces/#{piece.id}"
+  end
+
   private
+
   def piece_params
-    params.permit(:opus, :number, :type_of_piece, :nickname, :composition_date, :multiple_instruments, :main_instrument, :key_signature)
+    params.permit(piece: [:opus, :number, :type_of_piece, :nickname, :composition_date, :multiple_instruments, :main_instrument, :key_signature])
   end
 
 

a0f0cbea9a2cde7bd2a8ba678d47cc05d2b45220
Michael Brandt <msbrandt00@gmail.com>
2022-02-04 15:37:08 -0700

    Only show pieces where bool is true

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -16,4 +16,8 @@ class Piece < ApplicationRecord
   def self.ordered
     all.order(:type_of_piece, :opus, number: :asc)
   end
+
+  def self.multiple_instruments
+    where("multiple_instruments = true")
+  end
 end
